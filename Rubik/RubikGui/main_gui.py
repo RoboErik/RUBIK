4da2617883c8ad3c7bea25655b9109f7bcf22a4e
Erik <e@erikpasternak.com>
2017-09-30 11:38:34 -0700

    Add more assets and prototype a gear animation
    - Adds multiple gear icons for animating
    - Cycles through rotated gear images to animate

--- Rubik/RubikGui/main_gui.py
+++ Rubik/RubikGui/main_gui.py
@@ -1,7 +1,10 @@
 from tkinter import *
+import os, time, threading
 
-class Gui ():
+
+class Gui():
     master_ = None
+    gear_icons = []
 
     def __init__(self, master):
         super().__init__()
@@ -9,20 +12,42 @@ class Gui ():
         frame = Frame(master)
         frame.pack()
 
+        self.gear_icons.append(PhotoImage(file=os.path.join("Rubik", "Assets", "gear-1.png")))
+        self.gear_icons.append(PhotoImage(file=os.path.join("Rubik", "Assets", "gear-2.png")))
+        self.gear_icons.append(PhotoImage(file=os.path.join("Rubik", "Assets", "gear-3.png")))
+        self.gear_icons.append(PhotoImage(file=os.path.join("Rubik", "Assets", "gear-4.png")))
+        self.gear_icons.append(PhotoImage(file=os.path.join("Rubik", "Assets", "gear-5.png")))
+
         self.button = Button(
             frame, text="QUIT", fg="red", command=frame.quit
-            )
+        )
         self.button.pack(side=LEFT)
+
+        gear1 = PhotoImage(file=os.path.join("Rubik", "Assets", "gear-1.png"))
+        self.button.config(image=self.gear_icons[0])
+        gear2 = PhotoImage(file=os.path.join("Rubik", "Assets", "gear-2.png"))
+        self.button.curr_gear = 0
 
         self.hi_there = Button(frame, text="Hello", command=self.say_hi)
         self.hi_there.pack(side=LEFT)
+        image = PhotoImage(file=os.path.join("Rubik", "Assets", "rubik-time.png"))
+        self.hi_there.config(image=image)
+        self.hi_there.image = image
 
-    def say_hi(self):
+        self.change_gear()
+
+    def change_gear(self):
+        self.button.curr_gear = (self.button.curr_gear + 1) % len(self.gear_icons)
+        self.button.config(image=self.gear_icons[self.button.curr_gear])
+        threading.Timer(0.1, self.change_gear).start()
+
+    @staticmethod
+    def say_hi():
         print("hi there, everyone!")
 
-# root = Tk()
-#
-# app = Gui(root)
-#
-# root.mainloop()
-# root.destroy()
+        # root = Tk()
+        #
+        # app = Gui(root)
+        #
+        # root.mainloop()
+        # root.destroy()

486eb23450a7bed5b07789bf076f735639e76497
Erik <e@erikpasternak.com>
2017-10-28 11:37:31 -0700

    Add missing function for demo

--- Rubik/RubikSolver/rubik_solver.py
+++ Rubik/RubikSolver/rubik_solver.py
@@ -81,10 +81,23 @@ class RubikSolver(threading.Thread):
         # strip.setPixelColor(1, 0x000000)
         self._teardown()
 
+
 def rainbow(strip, wait_ms=20, iterations=1):
-	"""Draw rainbow that fades across all pixels at once."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((i+j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256 * iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i + j) & 255))
+        strip.show()
+        time.sleep(wait_ms / 1000.0)
+
+
+def wheel(pos):
+    """Generate rainbow colors across 0-255 positions."""
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
